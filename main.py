9f530e4ba5fca6d387b7e02d3efdd0387849ef8c
Siratee K. <sirateek@gmail.com>
2024-12-09 11:06:50 +0700

    Implement Simple Login Code

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("HelloWorld")
+password = "spsm"
+
+input_password = input("Please enter your password: ")
+
+if password == input_password:
+    print("Correct!")
+else:
+    print("Wrong password!")
